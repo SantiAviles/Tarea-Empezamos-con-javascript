3051cbd731d063a8a06971d5d0ca60573af766c3
Santiago <aviles.barrientos.santiago.sa@gmail.com>
2024-08-13 21:00:12 -0400

    añadir funcion de multiplicar

--- src/presenter.js
+++ src/presenter.js
@@ -1,9 +1,15 @@
 import sumar from "./sumador";
+import multiplicar from './multiplicador';
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
+
+const firstM = document.querySelector("#primer-numeroM");
+const secondM = document.querySelector("#segundo-numeroM");
+const formM = document.querySelector("#mult-form");
+const divM = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -13,3 +19,12 @@ form.addEventListener("submit", (event) => {
 
   div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
 });
+
+formM.addEventListener("submit", (event) =>{
+  event.preventDefault();
+
+  const firstNumber = Number.parseInt(firstM.value);
+  const secondNumber = Number.parseInt(secondM.value);
+
+  divM.innerHTML = "<p>" + multiplicar(firstNumber, secondNumber) + "</p>";
+})
--- src/sumador.test.js
+++ src/sumador.test.js
@@ -5,3 +5,4 @@ describe("Sumar", () => {
     expect(sumar(3, 2)).toEqual(5);
   });
 });
+
